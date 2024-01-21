923c57d6b2c8ee5b103082d105fb61b654d2d32e
Anshu Kumar <anshu121hjp@gmail.com>
2024-01-21 22:29:49 +0530

    service add

--- api/models/cartModel.js
+++ api/models/cartModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema(
   {
-    userId: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref:"User",
       required: true,
